6241226b180e57996d1f57de9874b1dddb4e3c1b
Ionut Antoniu <ioni_gherlan@yahoo.com>
2024-02-13 17:02:52 +0200

    Add dark theme mode

--- functions.js
+++ functions.js
@@ -37,3 +37,14 @@ function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/moon.png";
+  }
+};
