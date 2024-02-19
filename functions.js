c9e0bff41d1bf94d8c151c1b73662541090e086d
Ionut-Antoniu <ioni_gherlan@yahoo.com>
2024-02-19 21:04:43 +0200

    Commented the JS functions

--- functions.js
+++ functions.js
@@ -9,6 +9,7 @@ function addTask() {
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
     let span = document.createElement("span");
+    //\u00df represents the x sign in Unicode
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
@@ -29,17 +30,19 @@ listContainer.addEventListener(
   },
   false
 );
-
+//This function must be used whenever we modify something
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
+//Here we save the data we have inserted in a local storage
+//If we close the app and come back, we find the same things
 function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
 
 var icon = document.getElementById("icon");
-
+//Implementing the dark mode 
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
